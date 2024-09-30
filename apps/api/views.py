733b558ea15945014ad1d9c8b56e1249ca04ce89
Yaokun <yiukuenchu@gmail.com>
2024-09-30 14:39:35 +0930

    feat: implement courses view

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,7 +1,17 @@
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from apps.admission.models import Course
 
 from .serializers import CourseSerializer
 
 
 class ListCourses(APIView):
     serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = self.serializer_class(courses, many=True)
+        return Response(serializer.data)
