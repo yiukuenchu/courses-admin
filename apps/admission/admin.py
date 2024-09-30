321053487343a4e6d1e4941c32ff81976cb3aeec
Yaokun <yiukuenchu@gmail.com>
2024-09-30 13:16:25 +0930

    feat: register the models

--- apps/admission/admin.py
+++ apps/admission/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Course, Intake
 
 # Register your models here.
+class IntakeInline(admin.TabularInline):
+    model = Intake
+    extra = 1
+    
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [IntakeInline]
+
+@admin.register(Intake)
+class IntakeAdmin(admin.ModelAdmin):
+    list_display = ('course', 'start_date', 'end_date')
