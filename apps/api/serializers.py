d04d70a465b86bf163eb35ed8ba6ab4154e2f4eb
Yaokun <yiukuenchu@gmail.com>
2024-09-30 13:27:08 +0930

    feat: implement serializers for converting data types

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 
+class IntakeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Intake
+        fields = ['id', 'start_date', 'end_date']
 
 class CourseSerializer(serializers.ModelSerializer):
-    pass
+    intakes = IntakeSerializer(many=True, read_only=True)
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'intakes']
